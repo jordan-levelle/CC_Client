09be1626fc0a04fd9451f71749195351b5c6de12
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-06-27 10:40:16 -0400

    Example Prop update

--- client/src/components/ExampleProposal.jsx
+++ client/src/components/ExampleProposal.jsx
@@ -16,6 +16,8 @@ const ExampleProposal = () => {
   const [exampleProposal, setExampleProposal] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [newVote, setNewVote] = useState({ name: '', vote: '', comment: '' });
+  const [expandedRows, setExpandedRows] = useState({}); // State to track expanded rows
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768); // Initial check
 
   useEffect(() => {
     const fetchProposal = async () => {
@@ -31,6 +33,25 @@ const ExampleProposal = () => {
 
     fetchProposal();
   }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop) {
+      setExpandedRows({});
+    }
+  }, [isDesktop]);
 
   const updateVote = (index, newVoteValue) => {
     handleExistingVoteUpdate(index, newVoteValue, exampleProposal, setExampleProposal);
@@ -50,6 +71,10 @@ const ExampleProposal = () => {
 
   const newSubmission = () => {
     handleNewSubmission(exampleProposal, newVote, setExampleProposal, setNewVote);
+  };
+
+  const toggleDetails = (voteId) => {
+    setExpandedRows((prev) => ({ ...prev, [voteId]: !prev[voteId] }));
   };
 
   if (isLoading) {
@@ -86,50 +111,104 @@ const ExampleProposal = () => {
             </thead>
             <tbody>
               {exampleProposal.votes.map((vote, index) => (
-                <tr key={index}>
-                  <td>{vote.name}</td>
-                  <td>
-                    <div className="opinion-buttons">
-                      {['Agree', 'Neutral', 'Disagree', 'Block'].map((voteOption) => (
-                        <div
-                        key={voteOption}
-                        data-tooltip-id={`${voteOption.toLowerCase()}-tooltip`}
-                        data-tooltip-html={tooltips[voteOption]}
-                      >
-                        <button
-                          key={voteOption}
-                          type="button"
-                          className={vote.vote === voteOption ? 'selected' : ''}
-                          onClick={() => updateVote(index, voteOption)}
-                          data-tip={tooltips[voteOption]} // Add data-tip attribute
-                        >
-                          <FontAwesomeIcon icon={icons[voteOption]} /> {voteOption}
+                <React.Fragment key={index}>
+                  <tr>
+                    <td className="mobile-name-opinion-row">
+                      <div className="name-container">
+                        <span>{vote.name}</span>
+                      </div>
+                      <div className="opinion-container">
+                        <span className="show-mobile">
+                          {vote.vote && (
+                            <span className="opinion-label">
+                              <FontAwesomeIcon icon={icons[vote.vote]} /> {vote.vote}
+                            </span>
+                          )}
+                        </span>
+                      </div>
+                      <div className="toggle-button show-mobile">
+                        <button onClick={() => toggleDetails(index)} aria-label="Toggle Details">
+                          {expandedRows[index] ? 'Hide Details' : 'Details'}
                         </button>
-                        <Tooltip id={`${voteOption.toLowerCase()}-tooltip`} />
+                      </div>
+                    </td>
+                    <td className="hide-mobile">
+                      <div className="opinion-buttons">
+                        {['Agree', 'Neutral', 'Disagree', 'Block'].map((voteOption) => (
+                          <div
+                            key={voteOption}
+                            data-tooltip-id={`${voteOption.toLowerCase()}-tooltip`}
+                            data-tooltip-html={tooltips[voteOption]}
+                          >
+                            <button
+                              type="button"
+                              className={vote.vote === voteOption ? 'selected' : ''}
+                              onClick={() => updateVote(index, voteOption)}
+                              data-tip={tooltips[voteOption]} // Add data-tip attribute
+                            >
+                              <FontAwesomeIcon icon={icons[voteOption]} /> {voteOption}
+                            </button>
+                            <Tooltip id={`${voteOption.toLowerCase()}-tooltip`} />
+                          </div>
+                        ))}
+                      </div>
+                    </td>
+                    <td className="hide-mobile">
+                      <textarea
+                        type="text"
+                        value={vote.comment}
+                        onChange={(e) => updateComment(index, e.target.value)}
+                        placeholder='Explain your vote...'
+                      />
+                    </td>
+                    <td className="hide-mobile">
+                      <button onClick={() => deleteSubmission(index)}>Delete</button>
+                    </td>
+                  </tr>
+                  {expandedRows[index] && (
+                    <tr className="details-row show-mobile">
+                      <td colSpan="4">
+                        <div className="expanded-details">
+                          <div className="opinion-buttons">
+                            {['Agree', 'Neutral', 'Disagree', 'Block'].map((voteOption) => (
+                              <div
+                                key={voteOption}
+                                data-tooltip-id={`${voteOption.toLowerCase()}-tooltip`}
+                                data-tooltip-html={tooltips[voteOption]}
+                              >
+                                <button
+                                  type="button"
+                                  className={vote.vote === voteOption ? 'selected' : ''}
+                                  onClick={() => updateVote(index, voteOption)}
+                                  data-tip={tooltips[voteOption]} // Add data-tip attribute
+                                >
+                                  <FontAwesomeIcon icon={icons[voteOption]} /> {voteOption}
+                                </button>
+                                <Tooltip id={`${voteOption.toLowerCase()}-tooltip`} />
+                              </div>
+                            ))}
+                          </div>
+                          <textarea
+                            type="text"
+                            value={vote.comment}
+                            onChange={(e) => updateComment(index, e.target.value)}
+                            placeholder='Explain your vote...'
+                          />
+                          <button onClick={() => deleteSubmission(index)}>Delete</button>
                         </div>
-                      ))}
-                    </div>
-                  </td>
-                  <td>
-                    <textarea
-                      type="text"
-                      value={vote.comment}
-                      onChange={(e) => updateComment(index, e.target.value)}
-                      placeholder='Explain your vote...'
-                    />
-                  </td>
-                  <td>
-                    <button onClick={() => deleteSubmission(index)}>Delete</button>
-                  </td>
-                </tr>
+                      </td>
+                    </tr>
+                  )}
+                </React.Fragment>
               ))}
-              <tr>
+              <tr className="submit-section">
                 <td>
                   <input
                     type="text"
                     name="name"
                     value={newVote.name}
                     onChange={newTableEntry}
+                    placeholder="Name"
                   />
                 </td>
                 <td>
@@ -175,8 +254,3 @@ const ExampleProposal = () => {
 };
 
 export default ExampleProposal;
-
-
-
-
-
