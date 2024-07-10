ee04997a81e5c3555c5aa54cd495a0ea5261cd65
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-07-10 11:01:31 -0400

    Signup updates

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,42 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { passwordCriteria } from '../constants/TextConstants'
+import { passwordCriteria } from '../constants/TextConstants';
 import { useSignup } from '../hooks/useSignup';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { Navigate } from 'react-router-dom';
+import DOMPurify from 'dompurify';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [captchaInput, setCaptchaInput] = useState('');
-  const { signup, 
-          validatePassword, 
-          validateEmail, 
-          verificationPending, 
-          error, 
-          isLoading, 
-          passwordErrors, 
-          emailError} = useSignup();
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const { signup, validatePassword, validateEmail, verificationPending, error, isLoading, passwordErrors, emailError } = useSignup();
 
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
 
   const handleEmailChange = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-    validateEmail(email);
+    const sanitizedEmail = DOMPurify.sanitize(e.target.value);
+    setEmail(sanitizedEmail);
+    validateEmail(sanitizedEmail);
   };
 
   const handlePasswordChange = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-    // Trigger password validation
-    validatePassword(password);
+    const sanitizedPassword = DOMPurify.sanitize(e.target.value);
+    setPassword(sanitizedPassword);
+    validatePassword(sanitizedPassword);
+    setPasswordMatch(sanitizedPassword === confirmPassword);
   };
 
-  const handleConfirmPassword = (e) => {
-    setConfirmPassword(e.target.value);
+  const handleConfirmPasswordChange = (e) => {
+    const sanitizedConfirmPassword = DOMPurify.sanitize(e.target.value);
+    setConfirmPassword(sanitizedConfirmPassword);
+    setPasswordMatch(password === sanitizedConfirmPassword);
   };
 
   const handleSubmit = async (e) => {
@@ -66,21 +64,17 @@ const Signup = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required />
-            {emailError && <div className="error">{emailError}</div>}
+          <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required />
+          {emailError && <div className="error small-text">{emailError}</div>}
         </div>
         <div>
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required />
           <ul className="password-criteria">
-          Minumum of:
-          <li className="password-criteria-list">
-            
+            <li className="password-criteria-list">
               {passwordCriteria.map((criterion, index) => (
                 <React.Fragment key={criterion.key}>
-                  <span
-                    className={passwordErrors[criterion.key] ? 'valid' : 'invalid'}
-                  >
+                  <span className={passwordErrors[criterion.key] ? 'valid' : 'invalid'}>
                     {criterion.label}
                   </span>
                   {index !== passwordCriteria.length - 1 && <span> </span>}
@@ -91,7 +85,8 @@ const Signup = () => {
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} required />
+          <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
+          {!passwordMatch && <div className="error small-text">Passwords do not match</div>}
         </div>
         <div>
           <label htmlFor="captchaInput">Enter CAPTCHA:</label>
@@ -102,13 +97,13 @@ const Signup = () => {
             name="captchaInput"
             placeholder="Enter CAPTCHA"
             value={captchaInput}
-            onChange={(e) => setCaptchaInput(e.target.value)}
+            onChange={(e) => setCaptchaInput(DOMPurify.sanitize(e.target.value))}
             tabIndex="6"
             aria-label="CAPTCHA"
           />
         </div>
         <button type="submit" disabled={isLoading}>Sign up</button>
-        {error && <div className="error">{error}</div>}
+        {error && <div className="error small-text">{error}</div>}
       </form>
       <style jsx>{`
         .password-criteria {
@@ -116,7 +111,7 @@ const Signup = () => {
           padding: 0;
         }
         .password-criteria li {
-          font-size: 0.9rem;
+          font-size: 0.7rem;
           margin: 4px 0;
         }
         .password-criteria .valid {
@@ -124,6 +119,13 @@ const Signup = () => {
         }
         .password-criteria .invalid {
           color: red;
+        }
+        .error {
+          color: red;
+          margin-top: 4px;
+        }
+        .small-text {
+          font-size: 0.8rem;
         }
       `}</style>
     </div>
