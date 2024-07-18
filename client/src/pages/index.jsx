97c6d4d9f073c8be5bd2896336cc75da42975380
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-07-18 15:37:01 -0500

    index route fix

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -6,5 +6,5 @@ export { default as Teams } from './Teams';
 export { default as Settings } from './Settings';
 export { default as AuthPage } from './Authentication';
 export { default as Verification } from './Verification';
-export { default as VerifyLoadingPage } from './VerifyLoadingPage';
+export { default as VerifyLoadingPage } from './VerificationLoadingPage';
 export { default as Subscribe } from './Subscribe';
