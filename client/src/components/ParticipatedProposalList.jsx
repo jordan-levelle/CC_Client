7d92820391e210add4a31de2b08569cb9fba3692
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-09-10 10:01:59 -0500

    import error link

--- client/src/components/ParticipatedProposalList.jsx
+++ client/src/components/ParticipatedProposalList.jsx
@@ -1,4 +1,5 @@
-import React, { useState, Link } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
 import { useProposalsContext } from '../hooks/useProposalContext';
 
 // import { useAuthContext } from '../hooks/useAuthContext';
